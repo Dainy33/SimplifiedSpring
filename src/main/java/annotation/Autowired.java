13c283cf5198fa75170e0dfe867b51e8ca7334fd
dainy33 <m_dainy33@163.com>
2019-01-19 12:21:20 +0800

    AutoWired

--- src/main/java/annotation/Autowired.java
+++ src/main/java/annotation/Autowired.java
@@ -1,6 +1,14 @@
 package annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD,ElementType.METHOD,ElementType.PARAMETER,ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Autowired {
+    String value();
 }
 /**
  * @program: SimplifiedSpringFrameWork
